f543a1eab5b675580ae6ad6e0b1f3a462b3e1385
Javierszn <Javier.gariv@hotmail.com>
2026-08-06 13:08:52 -0600

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
-
 export const environment = {
   production: true,
-  apiUrl: 'https://api.registrocivil.slp.gob.mx/api' 
+  // Esta es la URL a la que Angular apuntará cuando esté en internet.
+  // Cámbienla por la dirección real (ej. la IP del servidor o el dominio de la dependencia)
+  apiUrl: 'https://www.tudominiooficial.gob.mx/api' 
 };
