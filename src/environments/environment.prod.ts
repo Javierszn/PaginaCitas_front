7b505ec8d056b7070e7379f3c40b444f98726540
Javierszn <Javier.gariv@hotmail.com>
2026-09-25 09:04:51 -0600

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   
   apiUrl: 'https://paginacitasback-production.up.railway.app/api',
-  recaptchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+  recaptchaSiteKey: '6Le3ts4tAAAAAAHEE4VgDKditLXcntMGH3zDwlGJ'
 };
